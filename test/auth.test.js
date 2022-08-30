d56f39e855e76a7e4124950391acef8989f5fb5e
Rami <aboomar.2@osu.edu>
2022-08-30 17:57:42 -0400

    added env and hide token

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,16 +1,14 @@
 const request = require("supertest");
 const app = require("../app");
 const DB = require("../config/db");
-
+require("dotenv").config();
 describe("Get /Auth user", () => {
   describe("given a valid token passed into headers", () => {
     test("should response with a 200 status code", async () => {
       const response = await request(app)
         .get("/api/v1/auth")
-        .set(
-          "x-auth-token",
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjMwZTdlYjk0ZmUzYWY1ODNlZjI1ZWQ3In0sImlhdCI6MTY2MTg5NDMzMCwiZXhwIjoxNjk3ODk0MzMwfQ.P0fSdfw9RywPwFjfMda1Aa50aXIPUxoh1UCX66A01_A"
-        );
+        .set("x-auth-token", process.env.AUTH_TOKEN);
+
       expect(response.statusCode).toEqual(200);
     });
   });
