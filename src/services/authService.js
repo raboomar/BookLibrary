0a8d64ed32da27a62e48759150fff42e5592fd2a
Rami <aboomar.2@osu.edu>
2022-08-31 10:50:12 -0400

    added login functionilty

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,7 @@
 const User = require("../models/Users");
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const getUser = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
@@ -14,6 +16,36 @@ const loginUser = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ errors: [{ msg: "invalid credentials" }] });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ errors: [{ msg: "invalid credentials" }] });
+    }
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+    jwt.sign(
+      payload,
+      config.get("jwtSecret"),
+      { expiresIn: 3600000 },
+      (error, token) => {
+        if (error) throw error;
+        res.json({ token });
+      }
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send(error.message);
   }
 };
 
