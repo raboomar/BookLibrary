ff18dfa0292355d708fa994a9338a9d04381c9de
Rami <aboomar.2@osu.edu>
2022-09-04 15:39:01 -0400

    created login page

--- client/src/auth/pages/LoginPage.js
+++ client/src/auth/pages/LoginPage.js
@@ -1,7 +1,67 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const handleOnChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+    const user = {
+      email,
+      password,
+
+      // registerUser(newUser);
+    };
+  };
+  return (
+    <section className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome back
+      </p>
+      <form className="form" onSubmit={(e) => handleSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => {
+              handleOnChange(e);
+            }}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={(e) => {
+              handleOnChange(e);
+            }}
+            required
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Create an account</Link>
+      </p>
+    </section>
+  );
 };
 
 export default LoginPage;
