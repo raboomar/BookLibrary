c7af48a45ff9bb0085a214b53b5b9c055c0c2559
Rami <aboomar.2@osu.edu>
2022-09-04 15:17:24 -0400

    added axios call to register user

--- client/src/auth/pages/RegisterPage.js
+++ client/src/auth/pages/RegisterPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./register.css";
 const RegisterPage = () => {
   const [formData, setFormData] = useState({
@@ -14,12 +15,33 @@ const RegisterPage = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (password != password2) {
       console.log("passwords do not match");
     } else {
       console.log(formData);
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const body = JSON.stringify(newUser);
+        const res = await axios.post(
+          "http://localhost:5001/api/v1/user",
+          body,
+          config
+        );
+        console.log(res.data);
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
