79216c5912e8a3842bd2e979f4aa1888a879463e
Rami <aboomar.2@osu.edu>
2022-09-05 14:13:42 -0400

    added logout to navbar and implemented logout & reset from redux

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
 import "./navbar.css";
 export const Navbar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
   return (
     <nav className="navbar bg-dark">
       <h1>
@@ -16,18 +29,34 @@ export const Navbar = () => {
             <span className="hide-sm">Dashboard</span>
           </Link>
         </li>
-        <li>
-          <Link to="/login">
-            <i className="fas fa-sign-out-alt"></i>
-            <span className="hide-sm">Login</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
-            <i className="fas fa-sign-out-alt"></i>
-            <span className="hide-sm">Register</span>
-          </Link>
-        </li>
+        {user ? (
+          <li>
+            <button
+              className="btn-logout"
+              onClick={() => {
+                handleLogout();
+              }}
+            >
+              <i className="fas fa-sign-out-alt"></i>
+              <span className="hide-sm">Logout</span>
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <i className="fas fa-sign-out-alt"></i>
+                <span className="hide-sm">Login</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <i className="fas fa-sign-out-alt"></i>
+                <span className="hide-sm">Register</span>
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
