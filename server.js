4da560de2def9f74547246eebb3f21177f91c498
Rami <aboomar.2@osu.edu>
2022-09-08 14:00:43 -0400

    trying again

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const PORT = process.env.PORT || 5001;
+var PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} `);
 });
