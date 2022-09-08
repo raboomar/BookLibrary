8f333e59831c05cd69c6d73019a7a4318eed8c09
Rami <aboomar.2@osu.edu>
2022-09-08 12:58:50 -0400

    preparing for deployment

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const v1BooksRouter = require("./src/routes/bookRoutes");
 const bodyParser = require("body-parser");
 const DB = require("./config/db");
 var cors = require("cors");
+const path = require("path");
+require("dotenv").config();
 app.use(cors());
 DB.connectDB();
 app.get("/", (req, res) => {
@@ -15,4 +17,13 @@ app.use(bodyParser.json());
 app.use("/api/v1/user", v1UserRouter);
 app.use("/api/v1/auth", v1AuthRouter);
 app.use("/api/v1/books", v1BooksRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "./", "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 module.exports = app;
